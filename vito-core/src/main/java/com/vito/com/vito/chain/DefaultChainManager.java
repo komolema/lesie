777b4ae3995a97eef532a60c74f8dffb29ccbe91
karabom <komolema@gmail.com>
2013-03-17 23:36:56 +0200

    vito-7: working on defining the bootsrap process

--- vito-core/src/main/java/com/vito/com/vito/chain/DefaultChainManager.java
+++ vito-core/src/main/java/com/vito/com/vito/chain/DefaultChainManager.java
@@ -2,6 +2,9 @@ package com.vito.com.vito.chain;
 
 import com.vito.com.vito.processor.Processor;
 import com.vito.framework.com.vito.framework.ChainManager;
+import com.vito.framework.com.vito.framework.Loader;
+
+import java.util.Queue;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,7 +13,9 @@ import com.vito.framework.com.vito.framework.ChainManager;
  * Time: 11:22 PM
  * To change this template use File | Settings | File Templates.
  */
-public class DefaultChainManager extends ChainManager {
+public class DefaultChainManager implements ChainManager {
+
+    private Queue<Processor<Loader>> processorQueue;
 
     @Override
     public void addProcessor(Processor processor) {
